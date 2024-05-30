1d93801e8a1881560c383069cbe0f3ca896f2310
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-05-30 09:48:08 +0530

    Input box UI fix

--- app/main/chat/page.tsx
+++ app/main/chat/page.tsx
@@ -16,6 +16,7 @@ import remarkGfm from 'remark-gfm'
 import rehype from 'rehype'
 
 const Main = () => {
+
     const { selected, setSelected } = useSidebar()
     useEffect(() => {
         setSelected(1)
@@ -29,11 +30,11 @@ const Main = () => {
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ ease: 'easeInOut', duration: 0.5 }}
-                    className="w-full border-2 border-black/10 p-5 lg:p-10 xl:px-16 min-h-screen"
+                    className="w-full border-2 flex flex-col items-center border-black/10 p-5 lg:p-10 xl:px-10 min-h-screen relative"
                 >
-                    <div className={cn("flex flex-col w-full py-10 gap-10")}>
+                    <div className={cn("flex flex-col w-full pb-10 gap-10")}>
                         {messages.map(m => (
-                            <div  key={m.id} className={cn("flex flex-col gap-5", m.role === 'user' ? "items-end" : "items-start")}>
+                            <div key={m.id} className={cn("flex flex-col gap-5", m.role === 'user' ? "items-end" : "items-start")}>
                                 <div className={cn("flex items-center gap-5", m.role === 'user' ? "flex-row-reverse" : "flex-row")}>
                                     <Image src={m.role === 'user' ? User : Olivia} width={40} height={40} alt={m.role === 'user' ? 'Rebhav Bharadwaj' : 'Olivia'} className="rounded-full h-[50px] w-[50px]" />
                                     <div>
@@ -46,23 +47,23 @@ const Main = () => {
                                 </Markdown>
                             </div>
                         ))}
-                        <form onSubmit={handleSubmit} className="relative w-full flex flex-col items-center justify-center">
-                            <div className="fixed items-center justify-around flex gap-5 bottom-0 border border-brandpurple rounded-lg shadow-md px-4 py-3 bg-white mb-8 w-full max-w-5xl">
-                                <Paperclip />
-                                <input
-                                    className="outline-none text-lg font-semibold w-full h-fit"
-                                    value={input}
-                                    placeholder="Type a message"
-                                    onChange={handleInputChange}
-                                />
-                                <button onClick={(e) => {
-                                    handleSubmit(e as any)
-                                }}>
-                                    <PaperPlaneRight size={32} />
-                                </button>
-                            </div>
-                        </form>
                     </div>
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center fixed bottom-0 mb-8 w-[60%]">
+                        <div className="items-center justify-around flex gap-5 border border-brandpurple rounded-lg shadow-md px-4 py-3 bg-white w-full">
+                            <Paperclip />
+                            <input
+                                className="outline-none text-lg font-semibold h-fit w-full text-wrap"
+                                value={input}
+                                placeholder="Type a message"
+                                onChange={handleInputChange}
+                            />
+                            <button onClick={(e) => {
+                                handleSubmit(e as any)
+                            }}>
+                                <PaperPlaneRight size={32} />
+                            </button>
+                        </div>
+                    </form>
                 </motion.div>
             </div>
         </div>
